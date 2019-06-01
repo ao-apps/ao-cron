3db453ca693e99b5ede9699e9f0be1bfb5c53a4e
AO Industries, Inc. <support@aoindustries.com>
2019-06-01 12:41:52 -0500

    Noted a sequential scan of a map that should be a key-based lookup.

--- src/main/java/com/aoindustries/cron/Matcher.java
+++ src/main/java/com/aoindustries/cron/Matcher.java
@@ -127,6 +127,9 @@ abstract public class Matcher {
 		return parseMatcher(str, 0, 7, 7, dayOfWeekNameMap);
 	}
 
+	// TODO: This is implemented as a interation for a case-insensitive.
+	//       Maybe this should instead put all items in the map lowercase, then lookup with toLowerCase.
+	//       If the case is required in the map, make a second map that stores all lowercased.
 	private static int parseInt(String str, Map<String,Integer> nameMap) throws NumberFormatException {
 		for(Map.Entry<String,Integer> entry : nameMap.entrySet()) {
 			if(entry.getKey().equalsIgnoreCase(str)) return entry.getValue();
